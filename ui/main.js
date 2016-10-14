7e1945b8919bbc7851f2568e23fedb55867fda81
amo2123 <amogha218@gmail.com>
2016-10-14 10:47:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Loaded!');
+var button=document.GetElementById("counter");
+ counter=0;
+button.OnClick=function(){
+   
+  counter=counter+1;
+  var span=document.GetElementById("count");
+  span.innerHtml=counter.toString();
+};
