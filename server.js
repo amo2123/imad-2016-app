c40be42b7f4f4d4af0118b522bd62449bd54fdac
amo2123 <amogha218@gmail.com>
2016-10-25 23:39:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
+var Pool = require('pg').Pool;
 var path = require('path');
+
+var config={
+    user:'amo2123',
+    database:'amo2123',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -13,7 +22,18 @@ app.get('/counter',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+    //
+    pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+;       } 
+else{
+    res.send(JSON.stringify(result));
+}
+    });
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
