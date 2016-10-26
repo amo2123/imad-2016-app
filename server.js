022e429267ce95efedfea8217e25d830fca455a1
amo2123 <amogha218@gmail.com>
2016-10-27 01:43:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get('/test-db',function(req,res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send(JSON.stringify(result.rows));
+            res.send(JSON.stringify(result.rows) +"<br><hr>"+ JSON.stringify(result));
         }
     });
 });
